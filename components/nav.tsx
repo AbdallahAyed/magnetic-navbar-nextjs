00a4a77a60efca3bb8e442277c6f5b6198586f0e
AbdallahAyed <abdalla.ayed.2010@gmail.com>
2024-03-07 19:39:48 -0800

    Add magnetic effect with mapRange func

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { motion, MotionValue, useMotionValue } from "framer-motion";
+
+import { cn } from "@/lib/utils";
 
 export function Nav() {
   const links = [
@@ -24,12 +25,53 @@ export function Nav() {
   const pathname = usePathname();
   const MotionLink = motion(Link);
 
+  const mapRange = (
+    inputLower: number,
+    inputUpper: number,
+    outputLower: number,
+    outputUpper: number
+  ) => {
+    const INPUT_RANGE = inputUpper - inputLower;
+    const OUTPUT_RANGE = outputUpper - outputLower;
+
+    return (value: number) =>
+      outputLower + (((value - inputLower) / INPUT_RANGE) * OUTPUT_RANGE || 0);
+  };
+
+  const setTransform = (
+    item: HTMLElement & EventTarget,
+    event: React.PointerEvent,
+    x: MotionValue,
+    y: MotionValue
+  ) => {
+    const bounds = item.getBoundingClientRect();
+    const relativeX = event.clientX - bounds.left;
+    const relativeY = event.clientY - bounds.top;
+
+    const xRange = mapRange(0, bounds.width, -1, 1)(relativeX);
+    const yRange = mapRange(0, bounds.height, -1, 1)(relativeY);
+
+    x.set(xRange * 10);
+    y.set(yRange * 10);
+    console.log(xRange);
+  };
+
   return (
     <nav className="p-8">
       <ul className="flex gap-12">
         {links.map((link) => {
+          const x = useMotionValue(0);
+          const y = useMotionValue(0);
+
           return (
-            <motion.li key={link.path}>
+            <motion.li
+              key={link.path}
+              onPointerMove={(event) => {
+                const item = event.currentTarget;
+                setTransform(item, event, x, y);
+              }}
+              style={{ x, y }}
+            >
               <MotionLink
                 href={link.path}
                 className={cn(
